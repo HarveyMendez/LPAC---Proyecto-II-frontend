de818e1bef9633ec7b3b5d740c02c4981865c4ac
irolaa <irolaluis20@gmail.com>
2025-06-27 19:31:28 -0600

    Estructura inicial, y archivos iniciales

---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPAC___Proyecto_II_frontend.Models
+{
+    internal class Cliente
+    {
+    }
+}
