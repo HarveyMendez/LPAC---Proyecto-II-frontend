2dd1c2322694d4b33b9c0b9fdfdf6c3ba48d8903
FquesadaC <fabian03qc@gmail.com>
2025-06-29 19:48:55 -0600

    Vista de orden de venta

---
+++ Views/OrdenWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LPAC___Proyecto_II_frontend.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LPAC___Proyecto_II_frontend.Views
+{
+    /// <summary>
+    /// Lógica de interacción para OrdenWindow.xaml
+    /// </summary>
+    public partial class OrdenWindow : UserControl
+    {
+        public OrdenWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new OrdenViewModel();
+        }
+    }
+}
